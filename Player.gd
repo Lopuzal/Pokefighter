831ad576480ca0b9488569b5390b30cae6cca0f1
Lopuzal <fuzo3000@outlook.fr>
2024-05-06 17:35:22 +0200

    better workflow, added input system and animations

--- Player.gd
+++ Player.gd
@@ -10,30 +10,22 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _physics_process(delta):
 	# Add the gravity.
-	if not is_on_floor():
-		velocity.y -= gravity * delta
+	
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	# var input_dir = Input.get_vector("move_left", "move_right", "move_left", "move_right")
-	
-	var direction = 0
-	
+
 	var is_moving_backwards = Input.is_action_pressed("move_left")
 	var is_moving_forwards = Input.is_action_pressed("move_right")
 	
-	if is_moving_backwards:
-		direction = -1
+	var currentRotation = transform.basis.get_rotation_quaternion()
+	velocity = currentRotation.normalized() * $AnimationTree.get_root_motion_position() / delta
 		
-	if is_moving_forwards:
-		direction = +1
-		
-	if direction != 0:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
-	get_node("AnimationTree").set("parameters/conditions/idle", !is_moving_backwards && !is_moving_forwards)
-	get_node("AnimationTree").set("parameters/conditions/walking_back", is_moving_backwards)
-	get_node("AnimationTree").set("parameters/conditions/walking", is_moving_forwards)
+	$AnimationTree.set("parameters/conditions/idle", !is_moving_backwards && !is_moving_forwards)
+	$AnimationTree.set("parameters/conditions/walking_back", is_moving_backwards)
+	$AnimationTree.set("parameters/conditions/walking", is_moving_forwards)
+	
+	if not is_on_floor():
+		velocity.y -= gravity * delta
 	move_and_slide()
--- main.gd
+++ main.gd
@@ -1,11 +1,12 @@
 extends Node3D
 
-
+var time = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	Engine.max_fps = 60
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	time += 1
+	$Label.text = str(time)
